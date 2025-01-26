c783f6d3be2fd7e3b9f8b512507540e5011b6ceb
Mutudu <aeondxf@live.com>
2025-01-26 23:15:55 +0800

    dev

--- src/Logger/PicoHex.Logger/AsyncBatchingLogSink.cs
+++ src/Logger/PicoHex.Logger/AsyncBatchingLogSink.cs
@@ -13,15 +13,13 @@ public class AsyncBatchingLogSink : ILogSink, IDisposable
             var batch = new List<string>(batchSize);
             while (!_cts.IsCancellationRequested)
             {
-                if (_queue.TryTake(out var message, 1000))
-                {
-                    batch.Add(message);
-                    if (batch.Count >= batchSize)
-                    {
-                        await innerSink.WriteAsync(string.Join(Environment.NewLine, batch));
-                        batch.Clear();
-                    }
-                }
+                if (!_queue.TryTake(out var message, 1000))
+                    continue;
+                batch.Add(message);
+                if (batch.Count < batchSize)
+                    continue;
+                await innerSink.WriteAsync(string.Join(Environment.NewLine, batch));
+                batch.Clear();
             }
         });
     }
