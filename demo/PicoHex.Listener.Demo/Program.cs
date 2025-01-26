7edf3f78bb9d7da82fa230cba3ffce6b8e27b77c
RomanDu <aeondxf@live.com>
2025-01-26 18:18:16 +0800

    dev

--- demo/PicoHex.Listener.Demo/Program.cs
+++ demo/PicoHex.Listener.Demo/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Net;
+using System.Text;
 using PicoHex.Tcp.Server;
 
 var tcpListener = new TcpListener(new IPEndPoint(IPAddress.Any, 8080));
@@ -16,12 +17,24 @@ tcpListener.Start();
 // 保持运行
 await Task.Delay(-1);
 
-await using var udpListener = new UdpListener(new IPEndPoint(IPAddress.Any, 514));
-udpListener.DataReceived += (data, ep) =>
-    Console.WriteLine($"Received {data.Length} bytes from {ep}");
-udpListener.ErrorOccurred += ex => Console.WriteLine($"Error: {ex.Message}");
-
+// 服务端代码（使用之前的UdpListener）
+var udpListener = new UdpListener(new IPEndPoint(IPAddress.Any, 514));
 udpListener.Start();
 
-// 保持运行
-await Task.Delay(Timeout.InfiniteTimeSpan);
+udpListener.DataReceived += (data, ep) =>
+{
+    Console.WriteLine($"Server received: {Encoding.UTF8.GetString(data)}");
+    _ = udpListener.SendAsync("Response"u8.ToArray(), ep);
+};
+
+// 客户端代码
+_ = Task.Run(async () =>
+{
+    await using var client = new UdpClient();
+    var response = await client.RequestAsync(
+        Encoding.UTF8.GetBytes("Hello"),
+        new IPEndPoint(IPAddress.Loopback, 514),
+        TimeSpan.FromSeconds(2)
+    );
+    Console.WriteLine($"Client got: {Encoding.UTF8.GetString(response)}");
+});
