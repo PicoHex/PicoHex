2af0ddcfa572f6e5c4199783bbe5ae1b2c86740f
RomanDu <aeondxf@live.com>
2025-01-26 17:38:11 +0800

    dev

---
+++ demo/PicoHex.Listener.Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using PicoHex.Tcp.Server;
+
+var tcpListener = new TcpListener(new IPEndPoint(IPAddress.Any, 8080));
+
+tcpListener.ClientConnected += clientId => Console.WriteLine($"Client connected: {clientId}");
+
+tcpListener.ClientDisconnected += clientId => Console.WriteLine($"Client disconnected: {clientId}");
+
+tcpListener.ErrorOccurred += ex => Console.WriteLine($"Error occurred: {ex}");
+
+tcpListener.Start();
+
+// 保持运行
+await Task.Delay(-1);
+
+await using var udpListener = new UdpListener(new IPEndPoint(IPAddress.Any, 514));
+udpListener.DataReceived += (data, ep) =>
+    Console.WriteLine($"Received {data.Length} bytes from {ep}");
+udpListener.ErrorOccurred += ex => Console.WriteLine($"Error: {ex.Message}");
+
+udpListener.Start();
+
+// 保持运行
+await Task.Delay(Timeout.InfiniteTimeSpan);
