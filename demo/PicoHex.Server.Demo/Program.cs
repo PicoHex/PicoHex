806110a43fc23202a9bc98eb9d9915c7325931d0
Mutudu <aeondxf@live.com>
2025-02-12 17:40:42 +0800

    dev

--- demo/PicoHex.Server.Demo/Program.cs
+++ demo/PicoHex.Server.Demo/Program.cs
@@ -1,95 +1,84 @@
-﻿namespace PicoHex.Server.Demo;
+﻿// Step 1: Create the IoC container
+using PicoHex.Server;
+using PicoHex.Server.Demo;
 
-internal static class Program
+var svcRegistry = ContainerBootstrap.CreateRegistry();
+
+// Registering logging
+// serviceCollection.AddLogging(builder => builder.AddConsole());
+
+// Registering Handlers
+svcRegistry.AddSingleton<ITcpHandler, MyStreamHandler>();
+svcRegistry.AddSingleton<IUdpHandler, MyBytesHandler>();
+svcRegistry.AddSingleton<ILogger<TcpServer>, Logger<TcpServer>>();
+svcRegistry.AddSingleton<ILogger<UdpServer>, Logger<UdpServer>>();
+svcRegistry.AddSingleton<ILogger<MyStreamHandler>, Logger<MyStreamHandler>>();
+svcRegistry.AddSingleton<ILogger<MyBytesHandler>, Logger<MyBytesHandler>>();
+svcRegistry.AddSingleton<ILoggerFactory>(_ =>
+    LoggerFactory.Create(builder => builder.AddConsole())
+);
+
+// Registering servers
+svcRegistry.AddSingleton<Func<ITcpHandler>>(sp => sp.Resolve<ITcpHandler>);
+svcRegistry.AddSingleton<Func<IUdpHandler>>(sp => sp.Resolve<IUdpHandler>);
+const int tcpPort = 12345;
+const int udpPort = 12346;
+svcRegistry.AddSingleton<TcpServer>(sp => new TcpServer(
+    IPAddress.Any,
+    tcpPort,
+    sp.Resolve<Func<ITcpHandler>>(),
+    sp.Resolve<ILogger<TcpServer>>()
+));
+svcRegistry.AddSingleton<UdpServer>(sp => new UdpServer(
+    IPAddress.Any,
+    udpPort,
+    sp.Resolve<Func<IUdpHandler>>(),
+    sp.Resolve<ILogger<UdpServer>>()
+));
+
+var serviceProvider = svcRegistry.CreateProvider();
+
+// Step 2: Try to get a logger from the IoC container
+// var logger = serviceProvider.GetService<ILogger<Program>>();
+
+// Step 3: If no logger is registered, set up a default console logger
+// if (logger == null)
+// {
+//     logger = CreateDefaultLogger<Program>();
+//     Console.WriteLine("No logger registered, using default console logger.");
+// }
+
+// Step 4: Set up the rest of your services
+try
 {
-    private static async Task Main(string[] args)
+    // Proceed with starting the servers (TcpServer and UdpServer)
+    var tcpServer = serviceProvider.Resolve<TcpServer>();
+    var udpServer = serviceProvider.Resolve<UdpServer>();
+
+    using var cancellationTokenSource = new CancellationTokenSource();
+
+    // Run servers
+    var tcpServerTask = tcpServer?.StartAsync(cancellationTokenSource.Token);
+    var udpServerTask = udpServer?.StartAsync(cancellationTokenSource.Token);
+
+    // Wait for servers to complete
+    await Task.WhenAny(tcpServerTask, udpServerTask).GetAwaiter().GetResult();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An unhandled exception occurred during application startup: {ex}");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
+
+// Creates a default logger that logs to the console
+ILogger<T> CreateDefaultLogger<T>()
+{
+    var factory = LoggerFactory.Create(builder =>
     {
-        // Step 1: Create the IoC container
-        var svcRegistry = ContainerBootstrap.CreateRegistry();
-
-        // Registering logging
-        // serviceCollection.AddLogging(builder => builder.AddConsole());
-
-        // Registering Handlers
-        svcRegistry.AddSingleton<ITcpHandler, MyStreamHandler>();
-        svcRegistry.AddSingleton<IUdpHandler, MyBytesHandler>();
-        svcRegistry.AddSingleton<ILogger<TcpServer>, Logger<TcpServer>>();
-        svcRegistry.AddSingleton<ILogger<UdpServer>, Logger<UdpServer>>();
-        svcRegistry.AddSingleton<ILogger<MyStreamHandler>, Logger<MyStreamHandler>>();
-        svcRegistry.AddSingleton<ILogger<MyBytesHandler>, Logger<MyBytesHandler>>();
-        svcRegistry.AddSingleton<ILoggerFactory>(
-            _ => LoggerFactory.Create(builder => builder.AddConsole())
-        );
-
-        // Registering servers
-        svcRegistry.AddSingleton<Func<ITcpHandler>>(sp => sp.Resolve<ITcpHandler>);
-        svcRegistry.AddSingleton<Func<IUdpHandler>>(sp => sp.Resolve<IUdpHandler>);
-        const int tcpPort = 12345;
-        const int udpPort = 12346;
-        svcRegistry.AddSingleton<TcpServer>(
-            sp =>
-                new TcpServer(
-                    IPAddress.Any,
-                    tcpPort,
-                    sp.Resolve<Func<ITcpHandler>>(),
-                    sp.Resolve<ILogger<TcpServer>>()
-                )
-        );
-        svcRegistry.AddSingleton<UdpServer>(
-            sp =>
-                new UdpServer(
-                    IPAddress.Any,
-                    udpPort,
-                    sp.Resolve<Func<IUdpHandler>>(),
-                    sp.Resolve<ILogger<UdpServer>>()
-                )
-        );
-
-        var serviceProvider = svcRegistry.CreateProvider();
-
-        // Step 2: Try to get a logger from the IoC container
-        // var logger = serviceProvider.GetService<ILogger<Program>>();
-
-        // Step 3: If no logger is registered, set up a default console logger
-        // if (logger == null)
-        // {
-        //     logger = CreateDefaultLogger<Program>();
-        //     Console.WriteLine("No logger registered, using default console logger.");
-        // }
-
-        // Step 4: Set up the rest of your services
-        try
-        {
-            // Proceed with starting the servers (TcpServer and UdpServer)
-            var tcpServer = serviceProvider.Resolve<TcpServer>();
-            var udpServer = serviceProvider.Resolve<UdpServer>();
-
-            var cancellationTokenSource = new CancellationTokenSource();
-
-            // Run servers
-            var tcpServerTask = tcpServer?.StartAsync(cancellationTokenSource.Token);
-            var udpServerTask = udpServer?.StartAsync(cancellationTokenSource.Token);
-
-            // Wait for servers to complete
-            await Task.WhenAny(tcpServerTask, udpServerTask).GetAwaiter().GetResult();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An unhandled exception occurred during application startup: {ex}");
-        }
-        finally
-        {
-            await Log.CloseAndFlushAsync();
-        }
-    }
-
-    // Creates a default logger that logs to the console
-    private static ILogger<T> CreateDefaultLogger<T>()
-    {
-        var factory = LoggerFactory.Create(builder =>
-        {
-            builder.AddConsole(); // This will log to the console by default
-        });
-        return factory.CreateLogger<T>();
-    }
+        builder.AddConsole(); // This will log to the console by default
+    });
+    return factory.CreateLogger<T>();
 }
