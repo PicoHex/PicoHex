4f1aa3e27583d7ce3a1f0bbf8f98a55fbf12d5d0
Mutudu <aeondxf@live.com>
2025-03-21 18:25:42 +0800

    dev

--- src/Logger/PicoHex.Logger.Json/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿// Global using directives
-
-global using System.Text.Json;
-global using System.Text.Json.Serialization;
-global using PicoHex.Logger.Abstractions;
--- src/Logger/PicoHex.Logger/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿// Global using directives
-
-global using System.Collections.Concurrent;
-global using System.Text;
-global using System.Threading.Channels;
-global using PicoHex.Logger.Abstractions;
