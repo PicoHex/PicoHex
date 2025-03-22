86506f42a56a34945a44a44114233d557961719c
Mutudu <aeondxf@live.com>
2025-03-23 00:40:51 +0800

    dev

--- src/Logger/PicoHex.Log/ConsoleLogSink.cs
+++ src/Logger/PicoHex.Log/ConsoleLogSink.cs
@@ -19,7 +19,7 @@ public class ConsoleLogSink : ILogSink
             try
             {
                 var formatted = _formatter.Format(entry);
-                Console.WriteLine(formatted);
+                WriteColoredLog(entry.Level, formatted);
             }
             catch
             { /* 确保主线程不受影响 */
@@ -31,6 +31,30 @@ public class ConsoleLogSink : ILogSink
         LogEntry entry,
         CancellationToken cancellationToken = default
     ) => await _channel.Writer.WriteAsync(entry, cancellationToken);
+
+    private void WriteColoredLog(LogLevel level, string message)
+    {
+        var originalColor = Console.ForegroundColor;
+
+        // 根据日志级别设置颜色
+        Console.ForegroundColor = level switch
+        {
+            LogLevel.Trace => ConsoleColor.Gray,
+            LogLevel.Debug => ConsoleColor.Cyan,
+            LogLevel.Info => ConsoleColor.Green,
+            LogLevel.Notice => ConsoleColor.Blue,
+            LogLevel.Warning => ConsoleColor.Yellow,
+            LogLevel.Error => ConsoleColor.Red,
+            LogLevel.Critical => ConsoleColor.DarkRed,
+            LogLevel.Alert => ConsoleColor.Magenta,
+            LogLevel.Emergency => ConsoleColor.DarkMagenta,
+            LogLevel.None => originalColor,
+            _ => originalColor
+        };
+
+        Console.WriteLine(message);
+        Console.ForegroundColor = originalColor; // 恢复原始颜色
+    }
 
     public void Dispose()
     {
