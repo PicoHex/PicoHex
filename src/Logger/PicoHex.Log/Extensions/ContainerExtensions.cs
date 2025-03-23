b5bc52412ca512ddc69f38058c1dd3df98033980
Mutudu <aeondxf@live.com>
2025-03-23 15:47:31 +0800

    dev

--- src/Logger/PicoHex.Log/Extensions/ContainerExtensions.cs
+++ src/Logger/PicoHex.Log/Extensions/ContainerExtensions.cs
@@ -4,10 +4,10 @@ public static class ContainerExtensions
 {
     public static ISvcContainer RegisterConsoleLogger<T>(this ISvcContainer container) =>
         container
-            .RegisterScoped<ILogFormatter, ConsoleLogFormatter>()
-            .RegisterScoped<ILoggerFactory, LoggerFactory>()
-            .RegisterScoped<ILogSink, ConsoleLogSink>()
-            .RegisterScoped<ILogger<T>, Logger<T>>();
+            .RegisterSingle<ILogFormatter, ConsoleLogFormatter>()
+            .RegisterSingle<ILoggerFactory, LoggerFactory>()
+            .RegisterSingle<ILogSink, ConsoleLogSink>()
+            .RegisterSingle<ILogger<T>, Logger<T>>();
 
     public static ILogger CreateLogger<T>(this ISvcContainer container) =>
         container.CreateProvider().Resolve<ILogger<T>>()!;
