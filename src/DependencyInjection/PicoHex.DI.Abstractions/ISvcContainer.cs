a9d25e78045d20916d7dd6f3b77b214e4b9aaa93
Mutudu <aeondxf@live.com>
2025-04-02 21:10:11 +0800

    dev

--- src/DependencyInjection/PicoHex.DI.Abstractions/ISvcContainer.cs
+++ src/DependencyInjection/PicoHex.DI.Abstractions/ISvcContainer.cs
@@ -4,6 +4,9 @@ public interface ISvcContainer : IDisposable, IAsyncDisposable
 {
     ISvcContainer Register(SvcDescriptor descriptor);
     ISvcProvider CreateProvider();
+    List<SvcDescriptor>? GetDescriptors(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type type
+    );
     SvcDescriptor? GetDescriptor(
         [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type type
     );
