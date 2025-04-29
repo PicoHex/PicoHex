297717c974e7ef4a6584347537ff04dcaebfe03b
Mutudu <aeondxf@live.com>
2025-04-29 17:19:39 +0800

    dev

--- src/Configuration/PicoHex.Configuration/StreamChangeToken.cs
+++ src/Configuration/PicoHex.Configuration/StreamChangeToken.cs
@@ -2,17 +2,47 @@ namespace PicoHex.Configuration;
 
 public class StreamChangeToken : IAsyncChangeToken
 {
-    private TaskCompletionSource<bool> _tcs = new();
+    private readonly Lock _syncLock = new();
+    private CancellationTokenSource _cts = new();
 
-    public bool HasChanged => _tcs.Task.IsCompleted;
+    public bool HasChanged { get; private set; }
 
-    public ValueTask WaitForChangeAsync(CancellationToken ct = default) =>
-        new(_tcs.Task.WaitAsync(ct));
+    public ValueTask WaitForChangeAsync(CancellationToken ct = default)
+    {
+        var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(ct, _cts.Token);
+        return new ValueTask(linkedCts.Token.WaitForCancellationAsync());
+    }
 
     public void NotifyChanged()
     {
-        var oldTcs = _tcs;
-        _tcs = new TaskCompletionSource<bool>();
-        oldTcs.TrySetResult(true);
+        lock (_syncLock)
+        {
+            if (_cts.IsCancellationRequested)
+            {
+                var oldCts = _cts;
+                _cts = new CancellationTokenSource();
+                oldCts.Dispose();
+            }
+            _cts.Cancel();
+            HasChanged = true;
+        }
+    }
+
+    public void Reset()
+    {
+        lock (_syncLock)
+        {
+            HasChanged = false;
+        }
+    }
+}
+
+public static class CancellationTokenExtensions
+{
+    public static Task WaitForCancellationAsync(this CancellationToken ct)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        ct.Register(() => tcs.TrySetResult(true));
+        return tcs.Task;
     }
 }
