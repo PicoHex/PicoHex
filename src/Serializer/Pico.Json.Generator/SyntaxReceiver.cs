a9615128ac2c60f0cca963e53d69caabe4dca7c3
Mutudu <aeondxf@live.com>
2025-05-07 18:35:00 +0800

    dev

--- src/Serializer/Pico.Json.Generator/SyntaxReceiver.cs
+++ src/Serializer/Pico.Json.Generator/SyntaxReceiver.cs
@@ -1,6 +1,18 @@
 ﻿namespace Pico.Json.Generator;
 
-public class SyntaxReceiver
+internal class JsonSyntaxReceiver : ISyntaxReceiver
 {
-    
+    public List<ClassDeclarationSyntax> CandidateClasses { get; } = new();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (
+            syntaxNode is ClassDeclarationSyntax classDecl
+            && classDecl.Modifiers.Any(m => m.IsKind(SyntaxKind.PublicKeyword))
+            && !classDecl.Modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword))
+        )
+        {
+            CandidateClasses.Add(classDecl);
+        }
+    }
 }
--- src/Serializer/Pico.Json/JsonWriter.cs
+++ src/Serializer/Pico.Json/JsonWriter.cs
@@ -1,1 +1,64 @@
-﻿
+﻿namespace Pico.Json;
+
+public static class JsonWriter
+{
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining
+    )]
+    public static void WriteString(System.Text.StringBuilder sb, string? value)
+    {
+        if (value == null)
+        {
+            sb.Append("null");
+            return;
+        }
+
+        sb.EnsureCapacity(sb.Length + value.Length * 6 + 2);
+        sb.Append('"');
+        foreach (var c in value)
+        {
+            switch (c)
+            {
+                case '"':
+                    sb.Append("\\\"");
+                    break;
+                case '\\':
+                    sb.Append("\\\\");
+                    break;
+                case '\b':
+                    sb.Append("\\b");
+                    break;
+                case '\f':
+                    sb.Append("\\f");
+                    break;
+                case '\n':
+                    sb.Append("\\n");
+                    break;
+                case '\r':
+                    sb.Append("\\r");
+                    break;
+                case '\t':
+                    sb.Append("\\t");
+                    break;
+                default:
+                    if (c < ' ')
+                        sb.Append($"\\u{(int)c:X4}");
+                    else
+                        sb.Append(c);
+                    break;
+            }
+        }
+        sb.Append('"');
+    }
+
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining
+    )]
+    public static void WriteNumber(System.Text.StringBuilder sb, int value) => sb.Append(value);
+
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining
+    )]
+    public static void WriteBool(System.Text.StringBuilder sb, bool value) =>
+        sb.Append(value ? "true" : "false");
+}
