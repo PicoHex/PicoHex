b2d61bb428035ad5a2037f843e56969d8f53639c
Mutudu <aeondxf@live.com>
2026-05-11 15:26:11 +0800

    clean

--- src/Node/Pico.Node.Core/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-// Global using directives
-
-global using System.Buffers;
-global using System.IO.Pipelines;
-global using System.Net;
-global using System.Net.Sockets;
--- src/Node/Pico.Node.Core/INode.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Pico.Node.Core;
-
-public interface INode : IAsyncDisposable
-{
-    ValueTask StartAsync(CancellationToken cancellationToken = default);
-    ValueTask StopAsync(CancellationToken cancellationToken = default);
-}
