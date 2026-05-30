5b4c04a94d0c584c052a0836a3566df74dfa7db4
Mutudu <aeondxf@live.com>
2026-05-30 08:40:11 +0800

    feat: scaffold PicoAop.Benchmarks project

---
+++ PicoAop/benchmarks/PicoAop.Benchmarks/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System.Diagnostics;
+global using PicoAop.Abs;
+global using PicoAop.DI;
+global using PicoBench;
+global using PicoBench.Formatters;
+global using PicoDI;
+global using PicoDI.Abs;
